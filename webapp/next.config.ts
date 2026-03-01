846338223401c8653ee8d82ac367f7ceaf020e44
test <test>
2026-03-01 19:26:20 +0900

    GitHub Pages

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/mogumogu-paimon",
 };
 
 export default nextConfig;
